2fc1e6e1cf1c8f0f30eca41209a349b6909daf2e
Kawshik Kumar Paul <kawshikbuet17@gmail.com>
2026-06-04 16:15:27 +0600

    Added OmniVoice TTS Inference execution sh file

---
+++ omnivoice/test_omnivoice_full_benchmark.sh
@@ -0,0 +1,13 @@
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
+export CUDA_VISIBLE_DEVICES=1
+
+CUDA_VISIBLE_DEVICES=1 python test_omnivoice_full_benchmark.py \
+  --suite all \
+  --model k2-fsa/OmniVoice \
+  --ref-audio ref_audio.wav \
+  --ref-text "বসের নির্দেশ, অন্য কাগজের কাছে খবরটা যাওয়ার আগেই আমাদেরকে কোনোভাবে স্পটে পৌছাতে হবে। অগত্যা তাই যেতেই হলো।" \
+  --instruct "female, young adult" \
+  --dtype fp16 \
+  --num-step 16 \
+  --speed 1.0 \
+  --output-dir outputs/omnivoice_full_benchmark_all_step16
